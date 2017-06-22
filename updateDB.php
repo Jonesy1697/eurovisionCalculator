bce7372d6505a4ff038a46e750a4b93f829e712a
Jonesy1697 <Joneschris1697@gmail.com>
2017-06-22 18:42:23 +0100

    Update to PHP

--- updateDB.php
+++ updateDB.php
@@ -2,35 +2,27 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$country12 = 'United Kingdom';
 $dbname = "eurovisioncalc";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname) or die("Connection failed: " . $conn->connect_error);
 // Check connection
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+$sql = "SELECT points FROM country WHERE country = '$country12'";
+$points = mysqli_query($conn, $sql) or die("Connection failed" . $conn->connect_error);
+$points = mysqli_fetch_row($points);
+$points = $points[0]; 
 
-$sql = "SELECT country FROM country WHERE final = 1";
+$sql = "UPDATE country
+		SET points = ($points + 12)
+		WHERE country = '$country12';";
+if ($conn->query($sql) === TRUE) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . $conn->error;
+}
 
-$result = mysqli_query($conn, $sql);
+$conn->close();
 
 ?>
-
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Eurovision scorer</title>
-	</head>
-	
-	<body>
-		<h1>Score countries</h1>
-		
-		<select>
-			<?php while($row1 = mysqli_fetch_array($result)):;?>
-			<option> <?php echo $row1[0];?></option>
-			<?php endwhile ?>
-		</select>
-	</body>
-</html>
