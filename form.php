63305c6b4f26fdbc0e7e57665cc5adee94d7dfba
Jonesy1697 <Joneschris1697@gmail.com>
2017-06-22 14:05:27 +0100

    Dynamic selection box and adding on database
    Addition of dynamic boxes to the main page
    
    Creation of php, to be integrated onto the main page

---
+++ form.php
@@ -0,0 +1,4 @@
+<form action = "updateDB.php" method = "post" />
+<p> Input 1: <input type = "text" name = "input1" /></p>
+<input type = "submit" value = "submit" />
+</form>
---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "eurovisioncalc";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT country FROM country WHERE final = 1";
+
+$result = mysqli_query($conn, $sql);
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Eurovision scorer</title>
+	</head>
+	
+	<body>
+		<h1>Score countries</h1>
+		
+		<select>
+			<?php while($row1 = mysqli_fetch_array($result)):;?>
+			<option> <?php echo $row1[0];?></option>
+			<?php endwhile ?>
+		</select>
+		
+		<input id = "button1" type = "submit" value = "Submit"/>	
+				
+	</body>
+</html>
