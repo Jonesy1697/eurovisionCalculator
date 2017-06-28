884dd8d3ce09554bb43afef1ae4545765b275889
Jonesy1697 <Joneschris1697@gmail.com>
2017-06-28 12:20:46 +0100

    Working voting system
    User can still vote with a refresh so needs fixing and styling

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if ($conn->connect_error) {
 
 $sql = "SELECT country FROM country WHERE final = 1";
 
-$result = mysqli_query($conn, $sql);
+$result = mysqli_query($conn, $sql); 
 
 ?>
 
@@ -27,13 +27,162 @@ $result = mysqli_query($conn, $sql);
 	<body>
 		<h1>Score countries</h1>
 		
-		<select>
-			<?php while($row1 = mysqli_fetch_array($result)):;?>
-			<option> <?php echo $row1[0];?></option>
-			<?php endwhile ?>
-		</select>
+		<form id="form1" name="form1" method="get" action="updateDB.php">
 		
-		<input id = "button1" type = "submit" value = "Submit"/>	
-				
+			<p>12 Points</p>
+		
+			<select name = "select[ ]">
+				<?php while($row1 = mysqli_fetch_array($result)):;?>
+				<option> <?php echo $row1[0];?></option>
+				<?php endwhile?>
+			</select>
+		
+			<br><br>
+			
+			<?php
+			$sql = "SELECT country FROM country WHERE final = 1";
+			$result = mysqli_query($conn, $sql); 
+			?>
+		
+			<p>10 Points</p>
+		
+			<select name = "select[ ]">
+				<?php while($row1 = mysqli_fetch_array($result)):;?>
+				<option> <?php echo $row1[0];?></option>
+				<?php endwhile?>
+			</select>
+		
+			<?php
+			$sql = "SELECT country FROM country WHERE final = 1";
+			$result = mysqli_query($conn, $sql); 
+			?>
+		
+			<br><br>
+			
+			<p>8 Points</p>
+		
+			<select name = "select[ ]">
+				<?php while($row1 = mysqli_fetch_array($result)):;?>
+				<option> <?php echo $row1[0];?></option>
+				<?php endwhile?>
+			</select>
+		
+			<?php
+			$sql = "SELECT country FROM country WHERE final = 1";
+			$result = mysqli_query($conn, $sql); 
+			?>
+		
+			<br><br>
+			
+			<p>7 Points</p>
+		
+			<?php
+			$sql = "SELECT country FROM country WHERE final = 1";
+			$result = mysqli_query($conn, $sql); 
+			?>
+		
+			<select name = "select[ ]">
+				<?php while($row1 = mysqli_fetch_array($result)):;?>
+				<option> <?php echo $row1[0];?></option>
+				<?php endwhile?>
+			</select>
+		
+			<br><br>
+			
+			<?php
+			$sql = "SELECT country FROM country WHERE final = 1";
+			$result = mysqli_query($conn, $sql); 
+			?>
+			
+			<p>6 Points</p>
+		
+			<select name = "select[ ]">
+				<?php while($row1 = mysqli_fetch_array($result)):;?>
+				<option> <?php echo $row1[0];?></option>
+				<?php endwhile?>
+			</select>
+		
+			<?php
+			$sql = "SELECT country FROM country WHERE final = 1";
+			$result = mysqli_query($conn, $sql); 
+			?>
+		
+			<br><br>
+			
+			<p>5 Points</p>
+		
+			<select name = "select[ ]">
+				<?php while($row1 = mysqli_fetch_array($result)):;?>
+				<option> <?php echo $row1[0];?></option>
+				<?php endwhile?>
+			</select>
+		
+			<?php
+			$sql = "SELECT country FROM country WHERE final = 1";
+			$result = mysqli_query($conn, $sql); 
+			?>
+			
+			<br><br>
+			
+			<p>4 Points</p>
+		
+			<select name = "select[ ]">
+				<?php while($row1 = mysqli_fetch_array($result)):;?>
+				<option> <?php echo $row1[0];?></option>
+				<?php endwhile?>
+			</select>
+		
+			<?php
+			$sql = "SELECT country FROM country WHERE final = 1";
+			$result = mysqli_query($conn, $sql); 
+			?>
+		
+			<br><br>
+			
+			<p>3 Points</p>
+		
+			<select name = "select[ ]">
+				<?php while($row1 = mysqli_fetch_array($result)):;?>
+				<option> <?php echo $row1[0];?></option>
+				<?php endwhile?>
+			</select>
+		
+			<?php
+			$sql = "SELECT country FROM country WHERE final = 1";
+			$result = mysqli_query($conn, $sql); 
+			?>
+		
+			<br><br>
+			
+			<p>2 Points</p>
+		
+			<select name = "select[ ]">
+				<?php while($row1 = mysqli_fetch_array($result)):;?>
+				<option> <?php echo $row1[0];?></option>
+				<?php endwhile?>
+			</select>
+		
+			<br><br>
+			
+			<?php
+			$sql = "SELECT country FROM country WHERE final = 1";
+			$result = mysqli_query($conn, $sql); 
+			?>
+			
+			<p>1 Points</p>
+		
+			<select name = "select[ ]">
+				<?php while($row1 = mysqli_fetch_array($result)):;?>
+				<option> <?php echo $row1[0];?></option>
+				<?php endwhile?>
+			</select>
+		
+			<br><br>
+			
+			<input id = "button1" type = "submit" value = "Submit"/>	
+	
+		</form>
+		
 	</body>
+	
 </html>
--- updateDB.php
+++ updateDB.php
@@ -4,23 +4,40 @@ $username = "root";
 $password = "";
 $country12 = 'United Kingdom';
 $dbname = "eurovisioncalc";
+$scoring = array("12", "10", "8", "7", "6", "5", "4", "3", "2", "1");
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname) or die("Connection failed: " . $conn->connect_error);
 // Check connection
 
-$sql = "SELECT points FROM country WHERE country = '$country12'";
-$points = mysqli_query($conn, $sql) or die("Connection failed" . $conn->connect_error);
-$points = mysqli_fetch_row($points);
-$points = $points[0]; 
+$values = $_GET['select'];
 
-$sql = "UPDATE country
-		SET points = ($points + 12)
-		WHERE country = '$country12';";
-if ($conn->query($sql) === TRUE) {
-    echo "Record updated successfully";
-} else {
-    echo "Error updating record: " . $conn->error;
+echo "<br>";
+
+$count = 0;
+
+foreach ($values as $a){
+	
+	$sql = "SELECT points FROM country WHERE country = '$a'";
+	$points = mysqli_query($conn, $sql) or die("Connection failed" . $conn->connect_error);
+	$points = mysqli_fetch_row($points);
+	$points = $points[0]; 
+
+	$sql = "UPDATE country
+			SET points = ($points + $scoring[$count])
+			WHERE country = '$a';";
+	if ($conn->query($sql) === TRUE) {
+		echo "Record updated successfully";
+	} else {
+		echo "Error updating record: " . $conn->error;
+	}
+	
+	echo "<b>" . $scoring[$count] . " points: </b>";
+	echo $a;
+	echo "<br>";
+	
+	$count = $count + 1;
+
 }
 
 $conn->close();
