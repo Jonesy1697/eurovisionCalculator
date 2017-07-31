fbc17baea6dabeb2ebd16d2596907270538ffde2
Jonesy1697 <Joneschris1697@gmail.com>
2017-07-31 13:27:35 +0100

    Comments added to the updateDB page
    Clarity added to code in te updataeDB page

--- updateDB.php
+++ updateDB.php
@@ -13,6 +13,7 @@
 
 	$count = 0;
 	
+	// Checks whether the user is able to vote
 	$sql = "SELECT voted FROM people WHERE name = '$user'";
 	$voted = mysqli_query($conn, $sql) or die("Connection failed" . $conn->connect_error);
 	$voted = mysqli_fetch_row($voted);
@@ -21,23 +22,24 @@
 	if ($voted == 0){
 	
 		foreach ($values as $a){
-					
+			// Gets the points for the selected country
 			$sql = "SELECT points FROM country WHERE country = '$a'";
 			$points = mysqli_query($conn, $sql) or die("Connection failed" . $conn->connect_error);
 			$points = mysqli_fetch_row($points);
 			$points = $points[0]; 
 		
+			// Adds on the points the user has assigned, to the current value in the database
 			$sql = "UPDATE country
 			SET points = ($points + $scoring[$count])
 			WHERE country = '$a';";
 		
+			// Runs the update query
 			$conn->query($sql);
 			$count = $count + 1;
 		}
 		
-
+		// Sets the current user to has voted
 		$sql = "UPDATE `people` SET `voted`= 1 WHERE name = '$user';";
-		
 		$conn->query($sql);
 		
 		$conn->close();
